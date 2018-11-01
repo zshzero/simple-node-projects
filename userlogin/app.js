aa8136e80a1654854270f635bc8c0648f5982f59
ashwinkumar3098@gmail.com <ashwinkumar3098@gmail.com>
2018-11-01 16:54:16 +0530

    User Login

---
+++ userlogin/app.js
@@ -0,0 +1,102 @@
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var expressValidator = require("express-validator");
+var multer = require("multer");
+var upload = multer({ dest: "./uploads" });
+var flash = require("connect-flash");
+var bcrypt = require("bcryptjs");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+var db = mongoose.connection;
+
+var routes = require("./routes/index");
+var users = require("./routes/users");
+
+var app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(flash());
+app.use(function(req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+
+app.get("*", function(req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
+app.use("/", routes);
+app.use("/users", users);
+
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
