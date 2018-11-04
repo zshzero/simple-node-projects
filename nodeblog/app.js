97724983946790441a3ba176e37a4dfe9e0079a4
Ashwin-A-K <ashwinkumar3098@gmail.com>
2018-11-04 05:21:31 -0600

    Updated nodeblog

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -13,7 +13,8 @@ var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var posts = require('./routes/posts');
+var categories = require('./routes/categories');
 
 var app = express();
 
@@ -71,7 +72,8 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/posts', posts);
+app.use('/categories', categories);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
