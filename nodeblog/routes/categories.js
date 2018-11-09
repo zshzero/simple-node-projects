add3330f20025c66c8594bd14934a250de40ad21
Ashwin-A-K <ashwinkumar3098@gmail.com>
2018-11-09 07:46:57 -0600

    nodeblog complete

--- nodeblog/routes/categories.js
+++ nodeblog/routes/categories.js
@@ -1,42 +1,53 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mongo = require('mongodb');
-var db = require('monk')('localhost/nodeblog');
+var mongo = require("mongodb");
+var db = require("monk")("localhost/nodeblog");
 
-router.get('/add', function(req, res, next) {
-	res.render('addcategory',{
-  		'title': 'Add Category'	
-	});
+router.get("/show/:category", function(req, res, next) {
+  var posts = db.get("posts");
+
+  posts.find({ category: req.params.category }, {}, function(err, posts) {
+    res.render("index", {
+      title: req.params.category,
+      posts: posts
+    });
+  });
 });
 
-router.post('/add', function(req, res, next) {
-  // Get Form Values
+router.get("/add", function(req, res, next) {
+  res.render("addcategory", {
+    title: "Add Category"
+  });
+});
+
+router.post("/add", function(req, res, next) {
   var name = req.body.name;
 
-  	// Form Validation
-	req.checkBody('name','Name field is required').notEmpty();
+  req.checkBody("name", "Name field is required").notEmpty();
 
-	// Check Errors
-	var errors = req.validationErrors();
+  var errors = req.validationErrors();
 
-	if(errors){
-		res.render('addpost',{
-			"errors": errors
-		});
-	} else {
-		var categories = db.get('categories');
-		categories.insert({
-			"name": name,
-		}, function(err, post){
-			if(err){
-				res.send(err);
-			} else {
-				req.flash('success','Category Added');
-				res.location('/');
-				res.redirect('/');
-			}
-		});
-	}
+  if (errors) {
+    res.render("addpost", {
+      errors: errors
+    });
+  } else {
+    var categories = db.get("categories");
+    categories.insert(
+      {
+        name: name
+      },
+      function(err, post) {
+        if (err) {
+          res.send(err);
+        } else {
+          req.flash("success", "Category Added");
+          res.location("/");
+          res.redirect("/");
+        }
+      }
+    );
+  }
 });
 
 module.exports = router;
--- nodeblog/routes/index.js
+++ nodeblog/routes/index.js
@@ -1,15 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mongo = require('mongodb');
-var db = require('monk')('localhost/nodeblog');
+var mongo = require("mongodb");
+var db = require("monk")("localhost/nodeblog");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	var db = req.db;
-	var posts = db.get('posts');
-	posts.find({}, {}, function(err, posts){
-		res.render('index', { posts: posts });
-	});
+router.get("/", function(req, res, next) {
+  var db = req.db;
+  var posts = db.get("posts");
+  posts.find({}, {}, function(err, posts) {
+    res.render("index", { posts: posts });
+  });
 });
 
 module.exports = router;
