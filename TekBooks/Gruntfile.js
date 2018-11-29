cad8ee675c322770c351683d991963a427e64893
ashwinkumar3098@gmail.com <ashwinkumar3098@gmail.com>
2018-11-29 10:58:07 +0530

    Updated tekbooks

--- TekBooks/Gruntfile.js
+++ TekBooks/Gruntfile.js
@@ -1,22 +1,20 @@
-"use strict";
+'use strict';
 
-module.exports = function(grunt) {
-    require("grunt-config-dir")(grunt, {
-        configDir: require("path").resolve("tasks")
+
+module.exports = function (grunt) {
+
+    // Load the project's grunt tasks from a directory
+    require('grunt-config-dir')(grunt, {
+        configDir: require('path').resolve('tasks')
     });
 
-    grunt.loadNpmTasks("grunt-makara-amdify");
-
+    
+        grunt.loadNpmTasks('grunt-makara-amdify');
+    
     // Register group tasks
-    grunt.registerTask("build", [
-        "eslint",
-        "eslint",
-        "dustjs",
-        "makara-amdify",
-        "less",
-        "requirejs",
-        "copyto"
-    ]);
+    grunt.registerTask('build', ['eslint', 'eslint', 'dustjs', 'makara-amdify', 'less', 'requirejs', 'copyto']);
 
-    grunt.registerTask("test", ["eslint", "mochacli"]);
+    grunt.registerTask('test', [ 'eslint', 'mochacli' ]);
+
+    
 };
--- TekBooks/controllers/index.js
+++ TekBooks/controllers/index.js
@@ -1,18 +1,23 @@
 'use strict';
 
-var IndexModel = require('../models/index');
-
+var Book = require('../models/bookModel');
 
 module.exports = function (router) {
+    router.get('/', function (req, res) {          
+         Book.find({}, function(err, books){
+         	if(err){
+         		console.log(err);
+         	}
 
-    var model = new IndexModel();
+         	books.forEach(function(book){
+         		book.truncText = book.truncText(50);
+         	});
 
-    router.get('/', function (req, res) {
-        
-        
-        res.render('index', model);
-        
-        
+         	var model = {
+         		books: books
+         	}
+         	res.render('index', model);
+         });    
     });
 
 };
