4c52da3ee086ae2c3597917aed09fd8e5e4908c1
Ashwin-A-K <ashwinkumar3098@gmail.com>
2018-12-30 04:26:13 -0800

    parsing cmd using minimist

--- Weather cli/index.js
+++ Weather cli/index.js
@@ -1,3 +1,15 @@
+const minimist = require("minimist");
+
 module.exports = () => {
-  console.log("Welcome!");
+  const args = minimist(process.argv.slice(2));
+  const cmd = args._[0];
+
+  switch (cmd) {
+    case "today":
+      require("./cmds/today")(args);
+      break;
+    default:
+      console.error(`"${cmd}" is not a valid command!`);
+      break;
+  }
 };
